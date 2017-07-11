92b33e64050135ff8a9b8760086c96b217286af3
labnize <zhulixh@126.com>
2017-07-11 19:46:10 +0800

    更换css3方法transform:scale

--- js/position.js
+++ js/position.js
@@ -1,5 +1,8 @@
 $(function () {
-  var markers = [];
+  $("#picDiv").css("height",$(window).height()-118+"px");
+  $(window).resize(function() {
+    $("#picDiv").css("height",$(window).height()-118+"px");
+  });
   $.getJSON("./testdata/position.json", function (data) {
     if(data.list && data.list.length > 0){
       var pos_bg = $(".pos-bg-img");
@@ -9,69 +12,12 @@ $(function () {
         $("#pin-img" + value.id).css({"left": value.left + "px", "top": value.top + "px"});
       });
 
-      markers = data.list;
-
-      var pos_pin = $(".pos-pin-img");
-      var bgWidth = Number(pos_bg.css("width").slice(0, pos_bg.css("width").length - 2));
-      var pinWidth = Number(pos_pin.css("width").slice(0, pos_pin.css("width").length - 2));
-      var bgWidthMin = bgWidth * 0.2;
-      var bgWidthMax = bgWidth * 1.8;
-      var base = 0.2;
-
       $(".enlarge").click(function () {
-        var bgCurrent = $(".pos-bg-img");
-        var pinCurrent = $(".pos-pin-img");
-        var bgWidthCurrent = Number(bgCurrent.css("width").slice(0, bgCurrent.css("width").length - 2));
-        var pinWidthCurrent = Number(pinCurrent.css("width").slice(0, pinCurrent.css("width").length - 2));
-        if(bgWidthCurrent < bgWidthMax){
-          var bg_width = bgWidthCurrent + bgWidth * base + "px";
-          var pin_width = pinWidthCurrent + pinWidth * base + "px";
-          bgCurrent.css({"width": bg_width});
-          pinCurrent.css({"width": pin_width});
-
-          var pinsArr = $(".pos-pin-img");
-          if(pinsArr && pinsArr.length > 0){
-            for(var i=0; i<pinsArr.length; i++){
-              var markerLeft = markers[i].left;
-              var markerTop = markers[i].top;
-              var markerCurrent = $("#pin-img" + i);
-              var markLeftCurrent = Number(markerCurrent.css("left").slice(0, markerCurrent.css("left").length - 2));
-              var markTopCurrent = Number(markerCurrent.css("top").slice(0, markerCurrent.css("top").length - 2));
-              markerCurrent.css({
-                "left": markLeftCurrent + markerLeft * base + "px",
-                "top": markTopCurrent + markerTop * base + "px"
-              });
-            }
-          }
-        }
+        ImageChange(true);
       });
 
       $(".diminsh").click(function () {
-        var bgCurrent = $(".pos-bg-img");
-        var pinCurrent = $(".pos-pin-img");
-        var bgWidthCurrent = Number(bgCurrent.css("width").slice(0, bgCurrent.css("width").length - 2));
-        var pinWidthCurrent = Number(pinCurrent.css("width").slice(0, pinCurrent.css("width").length - 2));
-        if(bgWidthCurrent > bgWidthMin){
-          var bg_width = bgWidthCurrent - bgWidth * base + "px";
-          var pin_width = pinWidthCurrent - pinWidth * base + "px";
-          bgCurrent.css({"width": bg_width});
-          pinCurrent.css({"width": pin_width});
-
-          var pinsArr = $(".pos-pin-img");
-          if(pinsArr && pinsArr.length > 0){
-            for(var i=0; i<pinsArr.length; i++){
-              var markerLeft = markers[i].left;
-              var markerTop = markers[i].top;
-              var markerCurrent = $("#pin-img" + i);
-              var markLeftCurrent = markerCurrent.css("left").slice(0, markerCurrent.css("left").length - 2);
-              var markTopCurrent = markerCurrent.css("top").slice(0, markerCurrent.css("top").length - 2);
-              markerCurrent.css({
-                "left": markLeftCurrent - markerLeft * base + "px",
-                "top": markTopCurrent - markerTop * base + "px"
-              });
-            }
-          }
-        }
+        ImageChange(false);
       });
 
       $(".pos").bind('mousewheel', function (event, delta) {
